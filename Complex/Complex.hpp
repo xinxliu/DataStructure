9a6696cb2a88576ad79aa8fe89dd2ad25f2eb456
xinxliu <lx1996@outlook.com>
2016-04-26 11:16:16 +0800

    add Complex

---
+++ Complex/Complex.hpp
@@ -0,0 +1,45 @@
+﻿#ifndef _TEST_H_X_
+#define _TEST_H_X_
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::ostream;
+template<typename T>
+class Complex {
+public:
+	typedef T value_type;
+	Complex(value_type real, value_type vir) :_real(real), _vir(vir) { cout << "this is the constructor" << endl; }
+	Complex() { cout << "this is the default constructor" << endl; }
+	Complex(Complex& a):_real(a._real),_vir(a._vir){}
+	Complex& operator=(const Complex& b) {
+		if (this == &b) {
+			return *this;
+		}
+		_real = b._real;
+		_vir = b._vir;
+		cout << "this is the operator=" << endl;
+		return *this;
+	}
+	~Complex(){
+		cout << "this is decontructor" << endl;
+	}
+	Complex add(Complex& b) {
+		/*Complex<T>* c = new Complex<T>(_real + b._real, _vir + b._vir);
+		return *c;*/
+		return Complex(_real + b._real, _vir + b._vir);
+	}
+	
+	template<typename U> friend ostream& operator<<(ostream& os, const Complex<U> a);
+private:
+	T _real;
+	T _vir;
+};
+
+template<typename U>
+ostream& operator<<(ostream& os, const Complex<U> a) {
+	os << a._real << " + i" << a._vir;
+	return os;
+}
+#endif
---
+++ Complex/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "Complex.hpp"
+using namespace std;
+
+
+void test(Complex<double>& a,Complex<double>& b){
+	Complex<double> c = a.add(b); 
+	cout << c;
+}
+int main(){
+	
+		Complex<double> a = Complex<double>(2.3, 4.4);
+		Complex<double> b = Complex<double>(5.3, 5.5);
+		test(a, b);
+	
+	getchar();
+	return 0;
+}
