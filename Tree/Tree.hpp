c55a4b11d5a233e5ae96b145a873fbe4ec1cdd8e
xinxliu <lx1996@outlook.com>
2016-05-14 22:31:02 +0800

    update Tree

--- Tree/Tree.hpp
+++ Tree/Tree.hpp
@@ -2,9 +2,34 @@
 #define _TREE_HPP_X_
 
 #include <iostream>
-class Tree {
+using std::cout;
+using std::cin;
+using std::endl;
+using std::cerr;
+
+template <typename T>
+struct BinaryTreeNode {
+	T _data;
+	BinaryTreeNode* _lchild;
+	BinaryTreeNode* _rchild;
+	BinaryTreeNode() :_lchild(NULL), _rchild(NULL) {}
+	BinaryTreeNode(T data,BinaryTreeNode* l = NULL,BinaryTreeNode* r = NULL):_data(x),_lchild(l),_rchild(r){}
+	print();
+};
+
+template<typename T>
+class BinaryTree {
+public:
+
+private:
+	BinaryTreeNode<T>* root;
 
 };
 
 
 #endif
+
+template<typename T>
+inline BinaryTreeNode<T>::print(){
+	cout << _data << " ";
+}
